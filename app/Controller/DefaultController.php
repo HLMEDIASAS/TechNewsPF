bc8054ebae3927e322e55bbef0aa18e69de8a708
Hugo LIEGEARD <hugo@hl-media.co>
2017-06-13 14:36:24 +0200

    Premier Versement

---
+++ app/Controller/DefaultController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Controller;
+
+use \W\Controller\Controller;
+
+class DefaultController extends Controller
+{
+
+	/**
+	 * Page d'accueil par défaut
+	 */
+	public function home()
+	{
+		$this->show('default/home');
+	}
+	
+	public function accueil() {
+	    $this->show('default/accueil');
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
---
+++ app/routes.php
@@ -0,0 +1,7 @@
+<?php
+	
+	$w_routes = array(
+		['GET', '/', 'Default#home', 'default_home'],
+	    ['GET', '/accueil.html', 'Default#accueil', 'default_accueil'],
+	    #['GET', '/clients/profil.html', 'Client#profil', 'client_profil'],
+	);
